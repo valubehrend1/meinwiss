7bb9d930b2c07457e865dc8e4c057114935e1e64
Bas <bastiansg.bas@gmail.com>
2024-11-26 14:31:21 -0300

    remove proxy

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,15 @@ export default defineConfig({
     plugins: [react()],
     server: {
         host: "0.0.0.0",
-        proxy: {
-            "/lupai": {
-                // target: "ws://190.16.250.34:8001",
-                target: "ws://localhost:8001",
-                changeOrigin: true,
-                secure: false,
-                ws: true,
-            },
-        },
+        // proxy: {
+        //     "/lupai": {
+        //         // target: "ws://190.16.250.34:8001",
+        //         target: "ws://localhost:8001",
+        //         changeOrigin: true,
+        //         secure: false,
+        //         ws: true,
+        //     },
+        // },
     },
     preview: {
         port: 4174,
