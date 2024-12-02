b903724bace2e5ffc4542a827828ae713c7bce72
valubehrend1 <valu-sp@hotmail.com>
2024-12-02 16:07:02 +0000

    Add dontate button, commented till we have donation page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -9,6 +9,7 @@ import HowlupaiWorksSection from './Sections/HowlupaiWorksSection/HowLupaiWorksS
 import HowLupaiWorksMobile from './Sections/HowlupaiWorksSection/HowLupaiWorksMobile';
 import LogosSection from './Sections/LogosSection/LogosSection';
 import GetInTouch from './Sections/GetInTouchSection/GetInTouch';
+/* import DonateButton from './DonateButton/DonateButton'; */
 import theme from '../../theme';
 
 const Home: React.FC = () => {
@@ -21,6 +22,7 @@ const Home: React.FC = () => {
       {matches ? <HowlupaiWorksSection /> : <HowLupaiWorksMobile />}
       <LogosSection />
       <GetInTouch />
+      {/*       <DonateButton /> */}
     </div>
   );
 };
