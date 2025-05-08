218c6c735e77d83acd743790196d637e80cb8383
Bas <bastiansg.bas@gmail.com>
2025-05-08 19:20:23 -0300

    add authentication token

--- src/context/WebSocketContext.tsx
+++ src/context/WebSocketContext.tsx
@@ -29,7 +29,7 @@ export const WebSocketProvider: React.FC<WebSocketProviderProps> = ({
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const wsInstance = new WebSocket('/lupai/multi_agent/chat');
+    const wsInstance = new WebSocket(`/lupai/multi_agent/chat?token=${import.meta.env.VITE_LUPAI_TOKEN}`);
     setWs(wsInstance);
 
     wsInstance.onopen = () => {
