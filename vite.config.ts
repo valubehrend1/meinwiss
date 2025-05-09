9097e69c895689e0d4bde63573d009ca0fed9876
Bas <bastiansg.bas@gmail.com>
2025-05-09 16:28:31 -0300

    upgrade websocket url

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,7 @@ export default defineConfig({
         host: "0.0.0.0",
         proxy: {
             "/lupai": {
-                // target: "ws://190.19.111.100:8001",
-                target: "ws://localhost:8001",
+                target: "ws://lupai.de:8000",
                 changeOrigin: true,
                 secure: false,
                 ws: true,
